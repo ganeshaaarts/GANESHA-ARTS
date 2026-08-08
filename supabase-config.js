52217c11db94df85662826d67194205cd5d84260
ganeshaaarts <marutimartofficial@gmail.com>
2026-08-08 17:12:53 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,12 +1,8 @@
 // Supabase Configuration File
-// Note: Isme apni Supabase URL aur Anon Key replace kar dena
+const SUPABASE_URL = "https://nojhnduyzjielryljtch.supabase.co"; 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vamhuZHV5emppZWxyeWxqdGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4OTc4OTYsImV4cCI6MjAzNzQ3Mzg5Nn0.aY66P6n0G_a08892_4u-i1N7n544u0O-hO4zP-xY"; 
 
-const SUPABASE_URL = "https://nojhnduyzjielryljtch.supabase.co/rest/v1/"; // e.g. https://xyz.supabase.co
-const SUPABASE_ANON_KEY = "sb_publishable_yrsM7YmUA2X1qvbYUCXUpw_pAUb6VVq"; 
+// Initialize Supabase Client
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Supabase Client Initialize
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Export/Global Access
-window.supabaseClient = supabase;
 console.log("Supabase connected successfully!");

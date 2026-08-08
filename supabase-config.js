4ac64eba3ef7b239dd27ca41e0206b26873e3566
ganeshaaarts <marutimartofficial@gmail.com>
2026-08-08 22:18:52 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -13,7 +13,7 @@
   }
 
   const SUPABASE_URL = 'https://nojhnduyzjielryljtch.supabase.co';
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vamhuZHV5emppZWxyeWxqdGNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4OTc4OTYsImV4cCI6MjAzNzQ3Mzg5Nn0.aY66P6n0G_a08892_4u-i1N7n544u0O-hO4zP-xY';
+  const SUPABASE_ANON_KEY = 'sb_publishable_yrsM7YmUA2X1qvbYUCXUpw_pAUb6VVq';
 
   window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
   console.log('Ganesha Arts: Supabase connected successfully.');
